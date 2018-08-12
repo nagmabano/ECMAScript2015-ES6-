4d73168370b76294a86e6c001e9e4809db33bd14
nagmabano <bano.nagma.1@gmail.com>
2018-08-12 22:47:07 +0530

    function scoped var and block scoped let and const

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //ES5
 
-var name5 = 'Jane Smith';
+/*var name5 = 'Jane Smith';
 var age5 = 26;
 name5 = 'Jane Miller';
 
@@ -13,9 +13,32 @@ console.log(name5);
 const name6 = 'Jane Smith'; // immutable variable
 let age6 = 23;
 name6 = 'Jane Miller';
-console.log(name6)  // gives error 'Assignment to constant variable'
+console.log(name6)  // gives error 'Assignment to constant variable'*/
+
+//ES5
+
+function driversLicence5(passedTest){
+    
+    if(passedTest){
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName + ' born in ' + yearOfBirth + ' is now officially allowed to drive a car.'); // works fine as var is function scoped
+}
+
+driversLicence5(true);
 
 
+//ES6
 
+function driversLicence6(passedTest){
+    
+    if(passedTest){
+        let firstName = 'John';
+        const yearOfBirth = 1990;
+    }
+     console.log(firstName + ' born in ' + yearOfBirth + ' is now officially allowed to drive a car.'); // gives error as let and const are block scoped
+}
 
+driversLicence6(true);
 
