776c0daae1a5909e43cc60b7cd3a9a527d359890
nagmabano <bano.nagma.1@gmail.com>
2018-08-13 11:33:03 +0530

    blocks and IIFE's

---
+++ app.js
@@ -0,0 +1,10 @@
+// Blocks and IIFE's
+
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+console.log(c); // 3
+//console.log(a+b); // reference error as a and b cannnot be accessed outside of a block 
