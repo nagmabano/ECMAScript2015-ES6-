de6792b7a81258d5f44ab1c0378502f611f8531f
nagmabano <bano.nagma.1@gmail.com>
2018-08-13 11:14:47 +0530

    let and var example

--- script.js
+++ script.js
@@ -35,16 +35,37 @@ driversLicence5(true);
 function driversLicence6(passedTest){
     
     if(passedTest){
-        console.log(firstName)  // throws error
+        //console.log(firstName)  // throws error variables are hoisted but we cannot use them before decleration
         let firstName = 'John';
         const yearOfBirth = 1990;
     }
-     console.log(firstName + ' born in ' + yearOfBirth + ' is now officially allowed to drive a car.'); // gives error as let and const are block scoped
+     //console.log(firstName + ' born in ' + yearOfBirth + ' is now officially allowed to drive a car.'); // gives error as let and const are block scoped
 }
 
 driversLicence6(true);
 
 //ES5
 
+var i =23;                    //<---    23                              //0 1 2 3 4 5 
+
+for(var i = 0; i < 5; i++){                    // value of i changes from 23 to 5
+    console.log(i);
+}
+
+console.log(i);               //<---    5
+
+//ES6
+
+let j = 23;                 //<---- 23
+
+for(let j = 0; j<5; j++){
+    console.log(j);                           // 0 1 2 3 4 23
+}
+
+console.log(j)             //<---- 23
 
 
+
+let k;
+
+console.log(k);
